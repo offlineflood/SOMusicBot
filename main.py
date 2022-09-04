7af0159a81c4fb51f97b256f712bd5e5ca3b11f1
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-09-04 21:48:14 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyrogram import Client as Bot
+from pyrogram import idle
+from callsmusic import run
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers")
+)
+
+bot.start()
+run()
+idle()
