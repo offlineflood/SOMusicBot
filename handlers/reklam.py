d14cba50ccc9ce721ff16c9a05dbb58cf8b111b9
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-09-07 22:50:23 +0400

    Update reklam.py

--- handlers/reklam.py
+++ handlers/reklam.py
@@ -7,24 +7,24 @@ from pyrogram.errors import UserAlreadyParticipant
 from callsmusic.callsmusic import client as aditya
 from config import SUDO_USERS
 
-@Client.on_message(filters.command(["reklam"]))
+@Client.on_message(filters.command(["advertisement"]))
 async def broadcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Reklam yayını başlıyor...`")
+        wtf = await message.reply("`Reklam başlayır...`")
         if not message.reply_to_message:
-            await wtf.edit("**__Lütfen mesajı bekleyiniz...__**")
+            await wtf.edit("**__Zəhmət olmasa mesajı gözləyin...__**")
             return
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
             try:
                 await aditya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Reklam başarıyla iletildi` \n\n**Gönderildiği yer:** `{sent}` Sohbet \n**Başarısız oldu:** {failed} Sohbet")
+                await wtf.edit(f"`Elan uğurla çatdırıldı` \n\n**Hara göndərilib:** `{sent}` Söhbət \n**Uğursuz oldu:** {failed} Sohbet")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Reklam başarıyla iletildi' \n\n**gönderildiği yer:** `{sent}` Sohbet \n**başarısız oldu:** {failed} Sohbet")
+        await message.reply_text(f"`Elan uğurla çatdırıldı' \n\n**Hara göndərilib:** `{sent}` Söhbət \n**Uğursuz oldu:** {failed} Sohbet")
