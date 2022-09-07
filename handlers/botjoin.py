5963e9760e5f86e3d89999940e1ebc16f8eae1c8
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-09-07 21:50:23 +0400

    Update botjoin.py

--- handlers/botjoin.py
+++ handlers/botjoin.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserAlreadyParticipant
 from helpers.decorators import errors, authorized_users_only
 
-@Client.on_message(filters.group & filters.command(["Qoşul", "asistan"]))
+@Client.on_message(filters.group & filters.command(["adding"]))
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -13,42 +13,42 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>Məni əvvəlcə admim etməlisiniz</b>",
+            "<b>ℹ️Məni əvvəlcə admim etməlisiniz.</b>",
         )
         return
 
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "Sesmusic Asistan"
+        user.first_name =  "𓄂𝙰𝚂𝚀|🇦🇿 𝓜𝓾𝓼𝓲𝓬 𝓐𝓼𝓲𝓼𝓼𝓽𝓪𝓷𝓽🦅"
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id,"Senin İsteğin Üzerine Geldim")
+        await USER.send_message(message.chat.id,"İstəyinizlə Gəldim...🥳")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>Asistan onsuzda qrupda var🙄</b>",
+            "<b>𓄂𝙰𝚂𝚀|🇦🇿 𝓜𝓾𝓼𝓲𝓬 𝓐𝓼𝓲𝓼𝓼𝓽𝓪𝓷𝓽🦅 onsuzda qrupda var.</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🔵 Vaxt aşımı xətası 🔵\n User {user.first_name} Userbot çoxlu qoşulma sorğularına görə qrupunuza qoşula bilmədi! Köməkçinin qrupda qadağan edilmədiyinə əmin olun."
-            "\n\n Yada asistan hesabını qrupa özün əlavə et </b>",
+            f"<b>📛Gözləmə xətası.\n User {user.first_name} qrupa daxil ola bilmədiyindən, bot musiqi oxuda bilmir.\n {user.first_name}, Qrupda əvvəlcədən qadağan olmadığını yoxlayın,sonra yenidən cəhd edin."
+            "\n\n və ya qrupa @ASQMusicAssistant hesabını əlavə edin. </b>",
         )
         return
     await message.reply_text(
-            "<b>Asistan onsuzda qrupda var🙄</b>",
+            "<b>𓄂𝙰𝚂𝚀|🇦🇿 𝓜𝓾𝓼𝓲𝓬 𝓐𝓼𝓲𝓼𝓼𝓽𝓪𝓷𝓽🦅 onsuzda qrupda var.</b>",
         )
     
-@USER.on_message(filters.group & filters.command(["ayril", "asistanby"]))
+@USER.on_message(filters.group & filters.command(["leave"]))
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>İstifadəçi qrupunuzdan ayrılamadı!."
-            "\n\nYada özün çıxara bilərsən</b>",
+            f"<b>İstifadəçi qrupunuzu tərk edə bilmədi!."
+            "\n\nvə ya özünüz qrupdan çıxarın.</b>",
         )
         return
  
