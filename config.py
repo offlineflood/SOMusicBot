f4daf0f081578f7605bc99796b4acdbb6cdec1fc
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2022-11-30 23:09:15 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "AgBGOwbX2BTpo0giW0MBpuDbwCcLZGvl4b-3acytp_Rvb9PM89c3o2JfyK171aGYsRxSoPCmXvXWoXRAONBMO11GbI5COj-kcdcefXQg4T3sHLuEgIHenXfIFYhj9DzQuqDqlGkzhG_fNq6Rj47pTKwyCRSEe7mvA7uXu8UMhofb0nJoZboqYbMJElHfp1Tpocv6JRFHF5tjD6C_EoPMeuaBMwATX8hk3F6nLAvk_xcPSAxym0pdEQw1rLB6uu580jSeD6QeCAecuj2pH765Dugch_NJ5Ma2WtWhOQqxdHh37YFEe3qfmhRLeKAsfaso0mDZbhqSqaSF38fRzpMLMZKSAAAAAT-zwaUA")
-BOT_TOKEN = getenv("BOT_TOKEN","5337183930:AAEQc8pbjleTSxzyjtS_GHYWfeVvcy4pEKo")
+SESSION_NAME = getenv("SESSION_NAME", "")
+BOT_TOKEN = getenv("BOT_TOKEN","")
 BOT_NAME = getenv("BOT_NAME", "Asisstant") 
-API_ID = int(getenv("API_ID","16157584"))
-API_HASH = getenv("API_HASH","2167d4e6007a79eed91d084bf5b8966a")
+API_ID = int(getenv("API_ID",""))
+API_HASH = getenv("API_HASH","")
 BOT_USERNAME = getenv("BOT_USERNAME", "SOmusiqi_Bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
